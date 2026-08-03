df6b05b9d1ae225a6baae655cb6176fa647f0b26
Lucas <lucaspfernandes.12@gmail.com>
2026-08-03 07:33:21 -0300

    Aprendi algumas funções básicas do Python, como print(), input() e a importância de declarar variáveis. Além disso, percebi que o Python é uma linguagem de programação de alto nível, fácil de aprender e muito utilizada em diversas áreas, como desenvolvimento web, ciência de dados e automação.

---
+++ 01-fundamentos/aula01_variaveis.py
@@ -0,0 +1,7 @@
+nome = "Lucas"
+idade = 26
+cidade = "Indaial"
+
+print(nome)
+print(idade)
+print(cidade)
---
+++ 01-fundamentos/aula02_input.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+cidade = input("Digite sua cidade: ")
+
+print("Olá, meu nome é", nome, ", tenho", idade, "anos e moro em", cidade)
